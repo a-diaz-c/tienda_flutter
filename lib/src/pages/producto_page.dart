84b3d29b41b8f31a53eb4a21b0d5e4d315ccc896
Daniela Barrientos <daniela@hotmail.com>
2020-08-06 12:28:53 -0500

    cuerpo pagina producto responsive

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -14,8 +16,7 @@ class ProductoPage extends StatefulWidget {
 class _ProductoPageState extends State<ProductoPage> {
   @override
   Widget build(BuildContext context) {
-    double _anchoContainerProducto = MediaQuery.of(context).size.width * 0.75;
-    double _anchoImagen = _anchoContainerProducto * 0.60;
+    double _anchoPantalla = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SingleChildScrollView(
           child: Column(
@@ -23,52 +24,95 @@ class _ProductoPageState extends State<ProductoPage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           navBar(),
-          Center(
-            child: Container(
-              width: _anchoContainerProducto,
-              padding: EdgeInsets.symmetric(vertical: 50.0),
-              child: Card(
-                margin: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        _imagenProducto(_anchoImagen),
-                        _textosProducto(),
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Flexible(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Especificaciones técnicas",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0),
-                                ),
-                                SizedBox(height: 10),
-                                _tablaTecnica(),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          _cardCalificacion(_anchoContainerProducto),
+          LayoutBuilder(builder: (context, constraints) {
+            if (MediaQuery.of(context).size.width > 900) {
+              return _cardProducto(_anchoPantalla * 0.75);
+            } else {
+              return _cardProductoMovil(_anchoPantalla * 0.90);
+            }
+          }),
+          _cardCalificacion(_anchoPantalla * 0.75),
           footer(),
         ],
       )),
+    );
+  }
+
+  Widget _cardProducto(double ancho) {
+    double anchoImagen = ancho * 0.60;
+    return Center(
+      child: Container(
+        width: ancho,
+        padding: EdgeInsets.symmetric(vertical: 50.0),
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Container(
+            padding: EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _imagenProducto(anchoImagen),
+                    _textosProducto(35.0),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Especificaciones técnicas",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18.0),
+                            ),
+                            SizedBox(height: 10),
+                            _tablaTecnica(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardProductoMovil(double ancho) {
+    return Center(
+      child: Container(
+        width: ancho,
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Container(
+            padding: EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _imagenProducto(ancho * 0.80),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _textosProducto(20.0),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -81,21 +125,27 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  Widget _textosProducto() {
+  Widget _textosProducto(double sizeTitulo) {
+    double textDatosProductos = sizeTitulo * 0.60;
     return Flexible(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _nombreProducto(),
+          _nombreProducto(sizeTitulo),
           SizedBox(height: 20.0),
           Container(
             child: Row(
               children: [
                 Text(
                   "Marca: ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: textDatosProductos),
                 ),
-                Text("TRUPER")
+                Text(
+                  "TRUPER",
+                  style: TextStyle(fontSize: textDatosProductos),
+                )
               ],
             ),
           ),
@@ -104,9 +154,14 @@ class _ProductoPageState extends State<ProductoPage> {
               children: [
                 Text(
                   "Marca: ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: textDatosProductos),
                 ),
-                Text("CMA0301050027")
+                Text(
+                  "CMA0301050027",
+                  style: TextStyle(fontSize: textDatosProductos),
+                )
               ],
             ),
           ),
@@ -115,60 +170,72 @@ class _ProductoPageState extends State<ProductoPage> {
               children: [
                 Text(
                   "Unidad de venta: ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: textDatosProductos),
                 ),
-                Text("Kilos")
+                Text(
+                  "Kilos",
+                  style: TextStyle(fontSize: textDatosProductos),
+                )
               ],
             ),
           ),
-          _iconos(Icons.local_shipping, "Envíos Gratis",
+          _iconos(
+              Icons.local_shipping,
+              "Envíos Gratis",
               "Monto mínimo de compra puede variar por tienda y zona",
+              sizeTitulo * 1.42,
               colorTexto: Colors.orange),
           SizedBox(height: 15.0),
           _iconos(Icons.verified_user, "Garantía Construrama",
-              "Seguridad en todas tus compras",
+              "Seguridad en todas tus compras", sizeTitulo * 1.42,
               colorTexto: Colors.green),
           SizedBox(height: 15.0),
-          _iconos(Icons.local_post_office, "Click & Pick up",
-              "Compra en línea y recoge tu pedido en tienda"),
+          _iconos(
+              Icons.local_post_office,
+              "Click & Pick up",
+              "Compra en línea y recoge tu pedido en tienda",
+              sizeTitulo * 1.42),
           SizedBox(height: 40.0),
-          _caracteristicasProducto(),
+          _caracteristicasProducto(sizeTitulo),
         ],
       ),
     );
   }
 
-  Widget _nombreProducto() {
+  Widget _nombreProducto(double size) {
     return Container(
       padding: EdgeInsets.only(top: 10.0),
       child: Text(
         'Truper, Clavo Negro 2" Para Concreto, Kilogramos',
-        style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold),
+        style: TextStyle(fontSize: size, fontWeight: FontWeight.bold),
       ),
     );
   }
 
-  Widget _caracteristicasProducto() {
+  Widget _caracteristicasProducto(double sizeTexto) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           child: Text(
             'Características',
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: sizeTexto * 0.571, fontWeight: FontWeight.bold),
           ),
         ),
         Container(
           child: Text(
             'Clavo negro para concreto de 2 pulgadas (50.8 Mm). Fabricado en acero al carbono. Con cuerpo rolado especial que facilita la penetración y proporciona mayor resistencia.',
-            style: TextStyle(),
+            style: TextStyle(fontSize: sizeTexto * 0.50),
           ),
         ),
       ],
     );
   }
 
-  Widget _iconos(IconData icono, String titulo, String subtitulo,
+  Widget _iconos(IconData icono, String titulo, String subtitulo, double size,
       {Color colorTexto}) {
     if (colorTexto == null) colorTexto = Colors.black;
     return Container(
@@ -176,7 +243,7 @@ class _ProductoPageState extends State<ProductoPage> {
         children: [
           Icon(
             icono,
-            size: 50.0,
+            size: size,
           ),
           Flexible(
             child: Column(
@@ -186,7 +253,7 @@ class _ProductoPageState extends State<ProductoPage> {
                   titulo,
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                      fontSize: size * 0.40,
                       color: colorTexto),
                 ),
                 Text(subtitulo)
@@ -259,56 +326,66 @@ class _ProductoPageState extends State<ProductoPage> {
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: ancho / 2,
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _puntiacion(),
-                          Row(
-                            children: [
-                              Text(
-                                "12 calificaciones",
-                                style: TextStyle(fontSize: 18.0),
-                              )
-                            ],
-                          ),
-                          _puntuajePorEstrella(5, 0.8, 10),
-                          _puntuajePorEstrella(4, 0.2, 2),
-                          _puntuajePorEstrella(3, 0.0, 0),
-                          _puntuajePorEstrella(2, 0, 0),
-                          _puntuajePorEstrella(1, 0, 0),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 20.0),
-                    Column(
+                LayoutBuilder(builder: (context, constraints) {
+                  print(MediaQuery.of(context).size.width);
+                  if (MediaQuery.of(context).size.width > 900) {
+                    return Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.only(right: 10.0),
-                                child: Text(
-                                  "Comentarios recientes",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )),
-                            InkWell(
-                              child: Text("Ver todos"),
-                              onTap: () => {},
-                            )
-                          ],
+                        Container(
+                          width: ancho / 2,
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _puntiacion(MainAxisAlignment.start, 50.0),
+                              _totalCalificaciones(MainAxisAlignment.start),
+                              _puntuajePorEstrella(
+                                  5, 0.8, 10, MainAxisAlignment.start, 100),
+                              _puntuajePorEstrella(
+                                  4, 0.2, 2, MainAxisAlignment.start, 100),
+                              _puntuajePorEstrella(
+                                  3, 0.0, 0, MainAxisAlignment.start, 100),
+                              _puntuajePorEstrella(
+                                  2, 0, 0, MainAxisAlignment.start, 100),
+                              _puntuajePorEstrella(
+                                  1, 0, 0, MainAxisAlignment.start, 100),
+                            ],
+                          ),
                         ),
-                        _comentario(),
-                        _comentario(),
+                        SizedBox(width: 20.0),
+                        _seccionComentarios(MainAxisAlignment.center),
                       ],
-                    ),
-                  ],
-                )
+                    );
+                  } else {
+                    print('ancho $ancho');
+                    return Column(
+                      children: [
+                        _puntiacion(MainAxisAlignment.center, 25.0),
+                        _totalCalificaciones(
+                          MainAxisAlignment.center,
+                        ),
+                        _puntuajePorEstrella(
+                            5, 0.8, 10, MainAxisAlignment.center, 100),
+                        _puntuajePorEstrella(
+                            4, 0.2, 2, MainAxisAlignment.center, 100),
+                        _puntuajePorEstrella(
+                            3, 0.0, 0, MainAxisAlignment.center, 100),
+                        _puntuajePorEstrella(
+                            2, 0, 0, MainAxisAlignment.center, 100),
+                        _puntuajePorEstrella(
+                            1, 0, 0, MainAxisAlignment.center, 100),
+                        InkWell(
+                          child: Text(
+                            "Ver todos los comentarios",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          onTap: () => {},
+                        )
+                      ],
+                    );
+                  }
+                }),
               ],
             ),
           ),
@@ -317,20 +394,21 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  Widget _puntiacion() {
+  Widget _puntiacion(MainAxisAlignment main, double size) {
     return Row(
+      mainAxisAlignment: main,
       children: [
         Text(
           "3.0",
           style: TextStyle(
-              fontSize: 50.0,
+              fontSize: size,
               fontWeight: FontWeight.bold,
               color: Colors.blue[900]),
         ),
         IconTheme(
           data: IconThemeData(
             color: Colors.amber,
-            size: 48,
+            size: size,
           ),
           child: StarDisplay(value: 3),
         )
@@ -338,28 +416,68 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  Widget _puntuajePorEstrella(
-      int numeroEstrella, double porcentaje, int comentarios) {
-    return Row(children: [
-      Icon(Icons.star),
-      Text(numeroEstrella.toString()),
-      LinearPercentIndicator(
-        width: 200.0,
-        lineHeight: 12.0,
-        percent: porcentaje,
-        progressColor: Colors.green[600],
-      ),
-      Text(comentarios.toString())
-    ]);
+  Widget _totalCalificaciones(MainAxisAlignment main) {
+    return Row(
+      mainAxisAlignment: main,
+      children: [
+        Text(
+          "12 calificaciones",
+          style: TextStyle(fontSize: 18.0),
+        )
+      ],
+    );
   }
 
-  Widget _comentario() {
+  Widget _puntuajePorEstrella(int numeroEstrella, double porcentaje,
+      int comentarios, MainAxisAlignment main, double ancho) {
+    return Row(
+      mainAxisAlignment: main,
+      children: [
+        Icon(Icons.star),
+        Text(numeroEstrella.toString()),
+        LinearPercentIndicator(
+          width: ancho,
+          lineHeight: 12.0,
+          percent: porcentaje,
+          progressColor: Colors.green[600],
+        ),
+        Text(comentarios.toString())
+      ],
+    );
+  }
+
+  Widget _seccionComentarios(MainAxisAlignment main) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: main,
+          children: [
+            Container(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  "Comentarios recientes",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+            InkWell(
+              child: Text("Ver todos"),
+              onTap: () => {},
+            )
+          ],
+        ),
+        _comentario(main),
+        _comentario(main),
+      ],
+    );
+  }
+
+  Widget _comentario(MainAxisAlignment main) {
     return Container(
       padding: EdgeInsets.all(5.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            mainAxisAlignment: main,
             children: [
               StarDisplay(
                 value: 5,
@@ -369,10 +487,12 @@ class _ProductoPageState extends State<ProductoPage> {
           ),
           SizedBox(height: 3.0),
           Row(
+            mainAxisAlignment: main,
             children: [Text("Satisfecho con mi compra")],
           ),
           SizedBox(height: 3.0),
           Row(
+            mainAxisAlignment: main,
             children: [
               Text("23/04/20"),
             ],
