b799f01df466c3d0b85bacf2900402d1d0b05948
Daniela Barrientos <daniela@hotmail.com>
2020-09-08 12:23:15 -0500

    tabla en pagina carrito

--- lib/src/pages/carrito_page.dart
+++ lib/src/pages/carrito_page.dart
@@ -49,59 +49,7 @@ class _CarritoPageState extends State<CarritoPage> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Navbar(),
-                  LayoutBuilder(builder: (context, constraints) {
-                    if (MediaQuery.of(context).size.width > 900) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width * 0.75,
-                        padding: EdgeInsets.symmetric(
-                            vertical: 20.0, horizontal: 40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ..._titulo(),
-                            ...cargarProductos(35.0),
-                            _botonActualizar(),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 20.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  _subtotal(),
-                                  _botonPago(),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    } else {
-                      return Container(
-                        width: MediaQuery.of(context).size.width * 0.75,
-                        padding: EdgeInsets.symmetric(
-                            vertical: 20.0, horizontal: 40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ..._titulo(),
-                            ...cargarProductos(20.0),
-                            _botonActualizar(),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 20.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  _subtotal(),
-                                ],
-                              ),
-                            ),
-                            _botonPago(),
-                          ],
-                        ),
-                      );
-                    }
-                  }),
+                  _cuerpo(),
                   footer(),
                 ],
               ),
@@ -112,30 +60,164 @@ class _CarritoPageState extends State<CarritoPage> {
     );
   }
 
-  List<Widget> _titulo() {
-    return [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget _cuerpo() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.75,
+      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          _titulo(),
           Container(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Carrito",
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
+            child: Container(
+              padding: EdgeInsets.only(right: 10.0),
+              width: MediaQuery.of(context).size.width,
+              height: 50 + (150 * datos.length).toDouble(),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                physics: ScrollPhysics(),
+                children: [
+                  DataTable(
+                      columnSpacing: 100,
+                      dataRowHeight: 150,
+                      sortColumnIndex: 2,
+                      sortAscending: false,
+                      columns: [
+                        DataColumn(label: Text("")),
+                        DataColumn(label: Text("")),
+                        DataColumn(label: Text("Producto")),
+                        DataColumn(label: Text("Cantidad"), numeric: true),
+                        DataColumn(
+                            label: Text("Precion Unitario"), numeric: true),
+                        DataColumn(label: Text("Total"), numeric: true),
+                      ],
+                      rows: rowProducto())
+                ],
               ),
             ),
           ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _subtotal(),
+                _botonPago(),
+              ],
+            ),
+          )
         ],
       ),
-      Divider(
-        color: Colors.grey,
-        height: 10,
-        thickness: 2.5,
-        indent: 20,
+    );
+  }
+
+  List<DataRow> rowProducto() {
+    subTotal = 0;
+    cantidadProductos = 0;
+    List<DataRow> dataRow = [];
+    datos.forEach(
+      (element) {
+        TextEditingController _myController = TextEditingController()
+          ..text = element['cantidad'].toString();
+        dataRow.add(
+          DataRow(
+            cells: [
+              DataCell(
+                IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    productosProviders.removeProductoCarrito(element['id']);
+                    _cargarCarrito();
+                    setState(() {});
+                  },
+                ),
+              ),
+              DataCell(
+                Container(
+                  width: 150,
+                  child: Image(
+                    image: NetworkImage(element['imagen']),
+                  ),
+                ),
+              ),
+              DataCell(Text(element['nombre'])),
+              DataCell(Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      if (element['cantidad'] > 1) {
+                        element['cantidad']--;
+                      }
+
+                      _myController.text = element['cantidad'].toString();
+                      productosProviders.actualizarCarrito(datos);
+                      setState(() {});
+                    },
+                  ),
+                  Container(
+                    width: 40,
+                    height: 25,
+                    child: TextField(
+                      controller: _myController,
+                      maxLength: 5,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 5.0,
+                        ),
+                        counterText: '',
+                      ),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12.0,
+                      ),
+                      inputFormatters: [
+                        WhitelistingTextInputFormatter.digitsOnly,
+                      ],
+                      onChanged: (texto) {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      int cantidad = element['cantidad']++;
+
+                      _myController.text = cantidad.toString();
+                      productosProviders.actualizarCarrito(datos);
+                      setState(() {});
+                    },
+                  ),
+                ],
+              )),
+              DataCell(Text(element['precio'].toString())),
+              DataCell(
+                  Text((element['precio'] * element['cantidad']).toString())),
+            ],
+          ),
+        );
+        subTotal += element['precio'] * element['cantidad'];
+        cantidadProductos += element['cantidad'];
+      },
+    );
+    return dataRow;
+  }
+
+  Widget _titulo() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Text(
+        "Carrito",
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-    ];
+    );
   }
 
   Widget _subtotal() {
@@ -169,234 +251,6 @@ class _CarritoPageState extends State<CarritoPage> {
     );
   }
 
-  List<Widget> cargarProductos(double sizeText) {
-    subTotal = 0;
-    cantidadProductos = 0;
-    List<Widget> filas = [];
-    for (var i = 0; i < datos.length; i++) {
-      if (sizeText >= 35.0) {
-        filas.add(_producto(i, sizeText));
-      } else {
-        filas.add(_productoMovil(i, sizeText));
-      }
-
-      filas.add(
-        Divider(
-          color: Colors.grey,
-          height: 2.5,
-          thickness: 1,
-          indent: 20,
-        ),
-      );
-      subTotal += datos[i]['precio'] * datos[i]['cantidad'];
-      cantidadProductos += datos[i]['cantidad'];
-    }
-    return filas;
-  }
-
-  Widget _producto(int index, double sizeText) {
-    TextEditingController _myController = TextEditingController()
-      ..text = datos[index]['cantidad'].toString();
-    double precio = datos[index]['precio'];
-    return Container(
-      padding: EdgeInsets.all(5.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 200,
-            height: 200,
-            child: Image(
-              image: NetworkImage(datos[index]['imagen']),
-            ),
-          ),
-          SizedBox(width: 10.0),
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: InkWell(
-                    child: Text(
-                      datos[index]['nombre'],
-                      style: TextStyle(
-                          fontSize: sizeText, fontWeight: FontWeight.normal),
-                    ),
-                    onTap: () {
-                      Navigator.pushNamed(
-                          context, 'producto/' + datos[index]['id']);
-                    },
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: SelectableText(
-                    'Precio  \$ ' + precio.toStringAsFixed(2),
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text("Cantidad "),
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        int cantidad = datos[index]['cantidad']++;
-
-                        _myController.text = cantidad.toString();
-                        setState(() {});
-                      },
-                    ),
-                    Container(
-                      width: 40,
-                      height: 25,
-                      child: TextField(
-                        controller: _myController,
-                        maxLength: 5,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 5.0,
-                            horizontal: 5.0,
-                          ),
-                          counterText: '',
-                        ),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 12.0,
-                        ),
-                        inputFormatters: [
-                          WhitelistingTextInputFormatter.digitsOnly,
-                        ],
-                        onChanged: (texto) {
-                          setState(() {});
-                        },
-                      ),
-                    ),
-                    IconButton(
-                        icon: Icon(Icons.remove),
-                        onPressed: () {
-                          if (datos[index]['cantidad'] > 1) {
-                            datos[index]['cantidad']--;
-                          }
-
-                          _myController.text =
-                              datos[index]['cantidad'].toString();
-                          setState(() {});
-                        }),
-                  ],
-                ),
-                _botonEliminar(index),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _productoMovil(int index, double sizeText) {
-    TextEditingController _myController = TextEditingController()
-      ..text = datos[index]['cantidad'].toString();
-    double precio = datos[index]['precio'];
-    return Container(
-      padding: EdgeInsets.all(5.0),
-      child: Row(
-        children: [
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image(
-                    image: NetworkImage(datos[index]['imagen']),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    datos[index]['nombre'],
-                    style: TextStyle(
-                        fontSize: sizeText, fontWeight: FontWeight.normal),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    'Precio  \$ ' + precio.toStringAsFixed(2),
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Cantidad "),
-                      IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () {
-                          int cantidad = datos[index]['cantidad']++;
-
-                          _myController.text = cantidad.toString();
-                          setState(() {});
-                        },
-                      ),
-                      Container(
-                        width: 40,
-                        height: 25,
-                        child: TextField(
-                          controller: _myController,
-                          maxLength: 5,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0)),
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 5.0,
-                              horizontal: 5.0,
-                            ),
-                            counterText: '',
-                          ),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 12.0,
-                          ),
-                          inputFormatters: [
-                            WhitelistingTextInputFormatter.digitsOnly,
-                          ],
-                          onChanged: (texto) {
-                            setState(() {});
-                          },
-                        ),
-                      ),
-                      IconButton(
-                          icon: Icon(Icons.remove),
-                          onPressed: () {
-                            if (datos[index]['cantidad'] > 1) {
-                              datos[index]['cantidad']--;
-                              _myController.text =
-                                  datos[index]['cantidad'].toString();
-                            }
-
-                            setState(() {});
-                          }),
-                    ],
-                  ),
-                ),
-                _botonEliminar(index),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _botonEliminar(int index) {
     return Container(
       height: 25,
@@ -413,25 +267,6 @@ class _CarritoPageState extends State<CarritoPage> {
           productosProviders.removeProductoCarrito(datos[index]['id']);
           _cargarCarrito();
           setState(() {});
-        },
-      ),
-    );
-  }
-
-  Widget _botonActualizar() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-            side: BorderSide(color: Colors.blue)),
-        child: Text(
-          "Actualizar",
-          style: TextStyle(color: Colors.blue, fontSize: 12.0),
-        ),
-        color: Colors.white,
-        onPressed: () {
-          productosProviders.actualizarCarrito(datos);
         },
       ),
     );
